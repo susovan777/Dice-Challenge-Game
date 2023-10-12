51e8aad93c4fe73dc73bb4fff6f81976c456dd95
Susovan <susovan.sahoo777@gmail.com>
2023-10-12 12:38:35 +0530

    Done with the 2nd dice image

--- script.js
+++ script.js
@@ -10,18 +10,32 @@
 
   */
 
+// 🚩🚩🚩🚩🚩🚩 For 1st dice image 🚩🚩🚩🚩🚩🚩
+
 // Task 1: Create a variable and assign random number generator
-    // we need numbers between 1 to 6
+// we need numbers between 1 to 6
 let randomNum1 = Math.floor(Math.random() * 6) + 1;
 console.log('Dice 1 number: ' + randomNum1);
 
 // Task 2: Create a string same as dice image file name
-    // creating a string using string litaral
+// creating a string using string litaral
 let randomImg1 = `images/dice${randomNum1}.png`;
 
 // Task 3: Select the dice image element
-    // selecting the 1st image element
+// selecting the 1st image element
 let randomDice1 = document.querySelector('.img1');
 
 // Task 4: Set the 'src' attribute
 randomDice1.setAttribute('src', randomImg1)
+
+// 🚩🚩🚩🚩🚩🚩 For 2nd dice image 🚩🚩🚩🚩🚩🚩
+
+// Repeating the same steps as we did for 1st dice
+let randomNum2 = Math.floor(Math.random() * 6) + 1;
+console.log('Dice 1 number: ' + randomNum2);
+
+let randomImg2 = `images/dice${randomNum2}.png`;
+t
+let randomDice2 = document.querySelector('.img2');
+
+randomDice2.setAttribute('src', randomImg2)
