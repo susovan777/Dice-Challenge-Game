9ad3353fcbcdfc0798cd0e1e9590a64c6d8d95f3
Susovan <susovan.sahoo777@gmail.com>
2023-10-12 12:34:31 +0530

    Updated html by adding the js file and done with the 1st dice implementation

--- script.js
+++ script.js
@@ -9,3 +9,19 @@
     - Make the winner
 
   */
+
+// Task 1: Create a variable and assign random number generator
+    // we need numbers between 1 to 6
+let randomNum1 = Math.floor(Math.random() * 6) + 1;
+console.log('Dice 1 number: ' + randomNum1);
+
+// Task 2: Create a string same as dice image file name
+    // creating a string using string litaral
+let randomImg1 = `images/dice${randomNum1}.png`;
+
+// Task 3: Select the dice image element
+    // selecting the 1st image element
+let randomDice1 = document.querySelector('.img1');
+
+// Task 4: Set the 'src' attribute
+randomDice1.setAttribute('src', randomImg1)
