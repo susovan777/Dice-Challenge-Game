393ab7fbc10fbad4e5f3a1d7d8e4f68f229e2ced
Susovan <susovan.sahoo777@gmail.com>
2023-10-12 12:22:30 +0530

    Updated TASKS to the script file

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Added js file to add dynamic nature 
+/**
+ TASKS: 
+    - Create a variable and assign random number generator
+    - Create a string same as dice image file name
+    - Select the dice image element
+    - Set the 'src' attribute
+    - Do the same steps for 2nd dice image also
+    - Make the winner
+
+  */
